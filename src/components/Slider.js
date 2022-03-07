ad95cf2095431cbe755e380e4037147b6660cc90
ovais <movaisr07@gmail.com>
2022-03-07 16:02:53 +0500

    effect functionality is done

---
+++ src/components/Slider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Slider  =({min,max, value, handleChange} )=>{
+   
+    return (
+    <div className='slider-container'>
+        <input
+          className='slider'
+          type="range" 
+          min ={min}
+          max={max}
+          value ={value}
+          onChange={handleChange}
+           />
+    </div>)
+}
+
+export default Slider
